d443da3ca974a9f052816c9c3058ba5255eb4740
redesquadrão16 <leila.bwt@gmail.com>
2022-11-06 23:44:04 -0300

    Prototipo Frontend React Agenia de Viagem - Pagina home

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -6,7 +6,6 @@ namespace projetomonitoria.Model
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        
         public DateTime DataNascimento { get; set; }
           
     }
---
+++ bwtagenciareact/src/components/Cards/CardHome.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+export default function CardHome(){
+    return( <div>
+
+
+<div className="col-12 col-md-10 offset-md-1" id="mini-banners">
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <div className="card text-center">
+                            <i className="bi bi-image-alt"> </i>
+                            <div className="card-body">
+                                <h5 className="card-title secondary-color">Lindos Lugares</h5>
+                                <p className="card-text secondary-color">Os melhores destinos e a melhor culinária local.
+                                </p>
+                                <a href="./destinos.html" class="btn btn-dark">Saber Mais</a>
+                            </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        <div className="row">
+                    <div className="col-12 col-md-4">
+                        <div className="card text-center">
+                            <i className="bi bi-image-alt"> </i>
+                            <div className="card-body">
+                                <h5 className="card-title secondary-color">Lindos Lugares</h5>
+                                <p className="card-text secondary-color">Os melhores destinos e a melhor culinária local.
+                                </p>
+                                <a href="./destinos.html" class="btn btn-dark">Saber Mais</a>
+                            </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        
+                    
+                    
+                    
+</div>
+    )
+}
